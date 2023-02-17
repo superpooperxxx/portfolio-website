bb7812aa7b699f9de83aca349c3a8c5c2f76ae55
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-02-17 15:22:58 +0200

    trying to fix hero section bug in safari mobile

--- src/index.js
+++ src/index.js
@@ -1,86 +1,86 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// gsap.timeline().from(
-//   '.hero__hi-svg',
-//   {
-//     duration: 2,
-//     strokeDashoffset: 629,
-//     delay: 0.1,
-//     ease: 'expo.in'
+gsap.timeline().from(
+  '.hero__hi-svg',
+  {
+    duration: 2,
+    strokeDashoffset: 629,
+    delay: 0.1,
+    ease: 'expo.in'
+  }
+)
+  .to(
+    '#svgGroup',
+    {
+      duration: 0.5,
+      delay: 0.1,
+      fill: '#D6D5A8',
+    }
+  )
+  .from(
+    '.hero__hi',
+    {
+      duration: 2,
+      left: '50%',
+      scale: 2,
+      transform: 'translateX(-50%)',
+      position: 'relative',
+      ease: 'power3.inOut',
+    }
+  )
+  .from(
+    '.hero__blob',
+    {
+      duration: 0.5,
+      opacity: 0,
+    }
+  )
+  .to(
+    '.hero__text-reveal',
+    {
+      duration: 0.5,
+      clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+      top: 0,
+      stagger: 0.5,
+      ease: 'power3.out'
+    }
+  )
+  .to(
+    '.hero__download-cv',
+    {
+      duration: 0.5,
+      delay: 1,
+      opacity: 1,
+      onComplete: () => document.querySelector('.page').classList.remove('page--loading')
+    }
+  );
+
+// gsap.timeline({
+//   scrollTrigger: {
+//     trigger: '.portfolio-header',
+//     // markers: true,
+//     start: 'top top',
+//     end: '+=50%',
+//     scrub: 1,
+//     pin: true,
 //   }
-// )
+// })
 //   .to(
-//     '#svgGroup',
+//     '.hero__container', 
 //     {
-//       duration: 0.5,
-//       delay: 0.1,
-//       fill: '#D6D5A8',
-//     }
-//   )
-//   .from(
-//     '.hero__hi',
-//     {
-//       duration: 2,
-//       left: '50%',
-//       scale: 2,
-//       transform: 'translateX(-50%)',
-//       position: 'relative',
-//       ease: 'power3.inOut',
-//     }
-//   )
-//   .from(
-//     '.hero__blob',
-//     {
-//       duration: 0.5,
+//       scale: 5,
 //       opacity: 0,
 //     }
 //   )
 //   .to(
-//     '.hero__text-reveal',
+//     '.hero',
 //     {
-//       duration: 0.5,
-//       clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
-//       top: 0,
-//       stagger: 0.5,
-//       ease: 'power3.out'
+//       opacity: 0,
 //     }
 //   )
 //   .to(
-//     '.hero__download-cv',
+//     '.hero',
 //     {
-//       duration: 0.5,
-//       delay: 1,
-//       opacity: 1,
-//       onComplete: () => document.querySelector('.page').classList.remove('page--loading')
+//       display: "none",
 //     }
-//   );
-
-gsap.timeline({
-  scrollTrigger: {
-    trigger: '.portfolio-header',
-    // markers: true,
-    start: 'top top',
-    end: '+=50%',
-    scrub: 1,
-    pin: true,
-  }
-})
-  .to(
-    '.hero__container', 
-    {
-      scale: 5,
-      opacity: 0,
-    }
-  )
-  .to(
-    '.hero',
-    {
-      opacity: 0,
-    }
-  )
-  .to(
-    '.hero',
-    {
-      display: "none",
-    }
-  )
+//   )
