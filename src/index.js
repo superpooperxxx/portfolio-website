723082d21d7f6158f09d98fd4c590e67244d746f
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-03-11 17:40:51 +0200

    added contacts section

--- src/index.js
+++ src/index.js
@@ -1,62 +1,47 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.timeline()
-  .to(
-    '.hero__hi-svg',
-    {
-      duration: 2,
-      strokeDashoffset: 0,
-      delay: 0.1,
-      ease: 'expo.in'
-    }
-  )
-  .to(
-    '#svgGroup',
-    {
-      duration: 0.5,
-      delay: 0.1,
-      fill: '#D6D5A8',
-    }
-  )
-  .to(
-    '.hero__hi',
-    {
-      duration: 2,
-      scale: 1,
-      left: 0,
-      transform: 'translateX(0)',
-      ease: 'power3.inOut',
-    }
-  )
-  .to(
-    '.hero__blob',
-    {
-      duration: 0.5,
-      opacity: 0.1,
-    }
-  )
-  .to(
-    '.hero__text-reveal',
-    {
-      duration: 0.5,
-      clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
-      top: 0,
-      stagger: 0.5,
-      ease: 'power3.out'
-    }
-  )
-  .to(
-    '.hero__download-cv',
-    {
-      duration: 0.5,
-      delay: 1,
-      opacity: 1,
-      onComplete: () => {
-        document.querySelector('.page').classList.remove('page--loading');
-        document.querySelector('.page__content').classList.remove('page__content--loading');
-      }
-    }
-  );
+gsap
+  .timeline()
+  .to(".hero__hi-svg", {
+    duration: 2,
+    strokeDashoffset: 0,
+    delay: 0.1,
+    ease: "expo.in",
+  })
+  .to("#svgGroup", {
+    duration: 0.5,
+    delay: 0.1,
+    fill: "#D6D5A8",
+  })
+  .to(".hero__hi", {
+    duration: 2,
+    scale: 1,
+    left: 0,
+    transform: "translateX(0)",
+    ease: "power3.inOut",
+  })
+  .to(".hero__blob", {
+    duration: 0.5,
+    opacity: 0.1,
+  })
+  .to(".hero__text-reveal", {
+    duration: 0.5,
+    clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+    top: 0,
+    stagger: 0.5,
+    ease: "power3.out",
+  })
+  .to(".hero__download-cv", {
+    duration: 0.5,
+    delay: 1,
+    opacity: 1,
+    onComplete: () => {
+      document.querySelector(".page").classList.remove("page--loading");
+      document
+        .querySelector(".page__content")
+        .classList.remove("page__content--loading");
+    },
+  });
 
 // gsap.timeline({
 //   scrollTrigger: {
@@ -69,7 +54,7 @@ gsap.timeline()
 //   }
 // })
 //   .to(
-//     '.hero__container', 
+//     '.hero__container',
 //     {
 //       scale: 5,
 //       opacity: 0,
@@ -88,99 +73,94 @@ gsap.timeline()
 //     }
 //   )
 
-gsap.timeline({
-  scrollTrigger: {
-    trigger: '.hero',
-    start: "top top",
-    scrub: true,
-  }
-})
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".hero",
+      start: "top top",
+      scrub: true,
+    },
+  })
   .to(
-    ['.hero__content', '.hero__hi'],
+    [".hero__content", ".hero__hi"],
     {
       y: 100,
     },
-    'start'
-  )
+    "start"
+  );
 
-const portfolioItems = gsap.utils.toArray('.portfolio__item');
+const portfolioItems = gsap.utils.toArray(".portfolio__item");
 
-portfolioItems.slice(0, portfolioItems.length - 1).forEach(item => {
-  gsap.to(
-    item,
-    {
-      background: '#9ea2cb',
-      scrollTrigger: {
-        trigger: item,
-        start: "top top",
-        pin: true,
-        pinSpacing: false,
-        scrub: true,
-        snap: 1,
-      }
-    }
-  )
-})
+portfolioItems.slice(0, portfolioItems.length - 1).forEach((item) => {
+  gsap.to(item, {
+    background: "#9ea2cb",
+    scrollTrigger: {
+      trigger: item,
+      start: "top top",
+      pin: true,
+      pinSpacing: false,
+      scrub: true,
+      snap: 1,
+    },
+  });
+});
 
-let newText = '';
-const thisPageText = document.querySelector('.this-page__text');
+let newText = "";
+const thisPageText = document.querySelector(".this-page__text");
 
-for (const word of thisPageText.innerText.split(' ')) {
+for (const word of thisPageText.innerText.split(" ")) {
   for (const letter of word) {
-    if (word === 'designed') {
+    if (word === "designed") {
       newText += `<span class="this-page__text-letter this-page__text-letter--accent_yellow">${letter}</span>`;
-    } else if (word === 'developed') {
+    } else if (word === "developed") {
       newText += `<span class="this-page__text-letter this-page__text-letter--accent_purple">${letter}</span>`;
     } else {
       newText += `<span class="this-page__text-letter">${letter}</span>`;
     }
   }
 
-  newText += ' ';
+  newText += " ";
 }
 
 thisPageText.innerHTML = newText;
 
-gsap.from(
-  '.this-page__text-letter',
-  {
-    opacity: 0,
-    color: '#816797',
-    stagger: 0.03,
-    scrollTrigger: {
-      trigger: '.this-page__text',
-      start: "10% bottom",
-      end: "bottom bottom",
-      toggleActions: "play play reverse none",
-      scrub: 1,
-    }
-  }
-)
-
-const thisPage = document.querySelector('.this-page');
-
-gsap.timeline({
+gsap.from(".this-page__text-letter", {
+  opacity: 0,
+  color: "#816797",
+  stagger: 0.03,
   scrollTrigger: {
-    trigger: '.wrapper',
-    start: 'top top',
-    end: '+=50%',
-    scrub: true,
-    pin: true,
-    markers: true,
-  }
-})
+    trigger: ".this-page__text",
+    start: "10% bottom",
+    end: "bottom bottom",
+    toggleActions: "play play reverse none",
+    scrub: 1,
+  },
+});
+
+const thisPage = document.querySelector(".this-page");
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".wrapper",
+      start: "top top",
+      end: "+=50%",
+      scrub: true,
+      pin: true,
+    },
+  })
   .to(
-    '.this-page',
+    ".this-page",
     {
       scale: 5,
       opacity: 0,
     },
-    'start'
+    "start"
   )
   .to(
-    '.contacts',
+    ".contacts",
     {
       opacity: 1,
     },
-    'start'
-  )
+    "start"
+  );
